fc6933f73d1fd28b9adadf8d759ffc5a98d8312e
b_silva <berna.loyola@gmail.com>
2016-02-02 03:32:09 +0000

    Updates up to week 5 day one of Finstagram tutorial - added log in and log out pages and updated actions to redirect respectively

--- app/actions.rb
+++ app/actions.rb
@@ -1,47 +1,64 @@
-def humanized_time_ago(time_ago_in_minutes)
-    if (time_ago_in_minutes >= 60)
-        "#{time_ago_in_minutes/60} hours ago"
-    else
-        "#{time_ago_in_minutes} minutes ago"
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
     end
 end
 
 get '/' do
-    post_shark = {
-        username: "sharky_j",
-        avatar_url: "http://naserca.com/images/sharky_j.jpg",
-        photo_url: "http://naserca.com/images/shark.jpg",
-        time_ago_in_minutes: humanized_time_ago(15),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "sharky_j",
-            text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
-        }]
-    }
-    post_whale = {
-        username: "kirk_whalum",
-        avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
-        photo_url: "http://naserca.com/images/whale.jpg",
-        time_ago_in_minutes: humanized_time_ago(65),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "kirk_whalum",
-            text: "#weekendvibes"
-        }]
-    }
-    post_marlin = {
-        username: "marlin_peppa",
-        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
-        photo_url: "http://naserca.com/images/marlin.jpg",
-        time_ago_in_minutes: humanized_time_ago(190),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "marlin_peppa",
-            text: "lunchtime! ;)"
-        }]
-    }
-    [post_shark, post_whale, post_marlin].to_s
+    @posts = Post.order(created_at: :desc)
+    erb :index
 end
+
+get '/signup' do
+    @user = User.new
+    erb :signup
+end
+
+post '/signup' do
+    
+    #collect user data from params hash
+    email = params[:email]
+    avatar_url = params[:avatar_url]
+    username = params[:username]
+    password = params[:password]
+    
+    @user = User.new({email: email, avatar_url: avatar_url, username: username, password: password})
+    
+    #if fields were entered correctly
+    if @user.save
+        redirect '/login'
+    else
+        erb :signup #display form
+    end
+end
+
+get '/login' do
+    erb :login
+end
+
+post '/login' do
+    
+    username = params[:username]
+    password = params[:password]
+    
+    user = User.find_by(username: username)
+    
+    if user && user.password == password
+        
+        session[:user_id] = user.id
+        redirect '/'
+
+    else
+        
+        @error_message = "Login failed."
+        erb :login
+        
+    end
+    
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect '/login'
+end
+
