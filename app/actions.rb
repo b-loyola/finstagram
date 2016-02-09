927948fc124a7dba5ce48bf954cafc983136c689
b_silva <berna.loyola@gmail.com>
2016-02-09 03:19:48 +0000

    Implemented likes and comments

--- app/actions.rb
+++ app/actions.rb
@@ -6,6 +6,7 @@ end
 
 get '/' do
     @posts = Post.order(created_at: :desc)
+    # @current_user = User.find_by(id: session[:user_id])
     erb :index
 end
 
@@ -33,6 +34,10 @@ post '/signup' do
 end
 
 get '/login' do
+    if session[:logout_message]
+        @logout_message = session[:logout_message]
+        session[:logout_message] = nil
+    end
     erb :login
 end
 
@@ -46,7 +51,7 @@ post '/login' do
     if user && user.password == password
         
         session[:user_id] = user.id
-        redirect '/'
+        redirect(to('/'))
 
     else
         
@@ -58,7 +63,64 @@ post '/login' do
 end
 
 get '/logout' do
-    session[:user_id] = nil
-    redirect '/login'
+    
+    if current_user
+        username = current_user.username
+        session[:user_id] = nil
+        session[:logout_message] = "User #{username} successfully logged out."
+    end
+    
+    redirect(to('/login'))
 end
 
+get '/posts/new' do
+    @post = Post.new
+    erb(:"posts/new")
+end
+
+post '/posts' do
+    photo_url = params[:photo_url]
+    
+    @post = Post.new({ photo_url: photo_url, user_id: current_user.id })
+    
+    if @post.save
+        redirect(to('/'))
+    else
+        erb(:"posts/new")
+    end
+end
+
+get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb(:"posts/show")
+end
+    
+post '/comments' do
+    comment_text = params[:text]
+    post_id = params[:post_id]
+    
+    comment = Comment.new({text: comment_text, user_id: current_user.id, post_id: post_id})
+    
+    comment.save
+    
+    redirect(back)
+    
+end
+
+post '/likes' do
+    post_id = params[:post_id]
+    
+    like = Like.new({ user_id: current_user.id, post_id: post_id})
+    
+    like.save
+    
+    redirect(back)
+    
+end
+
+delete '/likes/:id' do
+    like = Like.find_by( id: params[:id])
+    like.destroy
+    
+    redirect(back)
+end
